c44fad92ff8d77fbbe5bc8233bcb0837caeeab94
rajpalrajpal <amanvirb2@gmail.com>
2019-08-15 20:32:42 +0200

    Create and Consume messages Successfully

---
+++ MicroRabbit.Transfer.Data/Context/TranferDbContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MicroRabbit.Transfer.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace MicroRabbit.Transfer.Data.Context
+{
+    public class TransferDbContext : DbContext
+    {
+        public TransferDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<TransferLog> TransferLogs { get; set; }
+    }
+}
---
+++ MicroRabbit.Transfer.Domain/Models/TransferLog.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MicroRabbit.Transfer.Domain.Models
+{
+    public class TransferLog    
+    {
+        public int Id { get; set; }
+        public string FromAccount { get; set; }
+        public string ToAccount { get; set; }
+        public decimal TransferAmount { get; set; }
+    }
+}
